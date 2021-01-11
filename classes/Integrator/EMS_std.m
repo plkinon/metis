9876271cc4164b2bd4cf6c76a8060441e2ce8150
Philipp <philipp.kinon@student.kit.edu>
2021-01-11 13:31:51 +0100

    working tangent matrix (missing break and 1/2 factor)

--- classes/Integrator/EMS_std.m
+++ classes/Integrator/EMS_std.m
@@ -18,7 +18,7 @@ classdef EMS_std < Integrator
     methods
         function self = initialise(self,~,this_problem)
             self.NAME  = 'EMS-std';
-            self.nVARS = 2*this_problem.nDOF+this_problem.mCONSTRAINTS;
+            self.nVARS = 2*this_problem.nDOF+1*this_problem.mCONSTRAINTS;
             self.LM0   = zeros(this_problem.mCONSTRAINTS,1);
         end
         
@@ -42,17 +42,13 @@ classdef EMS_std < Integrator
             %% Known quantities from last time-step
             qn      = zn(1:n);
             pn      = zn(n+1:2*n);
-            lambdan = zn(2*n+1:2*n+m);
             
             %% MP evaluated quantities
             q_n05      = 0.5*(qn + qn1);
             p_n05      = 0.5*(pn + pn1);
-            lambda_n05 = 0.5*(lambdan + lambda_n1);
             DVext_n05  = this_problem.external_potential_gradient(q_n05);
             D2Vext_n05 = this_problem.external_potential_hessian(q_n05);
             D2Vint_n05 = this_problem.internal_potential_hessian(q_n05);
-            G_n05      = this_problem.constraint_gradient(q_n05);
-            t_n05      = zeros(n);
             
             %% Discrete gradients
             % for the internal potential
@@ -81,14 +77,17 @@ classdef EMS_std < Integrator
                     % discrete gradient
                     DG_Vint  = DG_Vint + (Vs_n1-Vs_n)/(pi_n1-pi_n)*DPiq_n05;
                     K21_DG_V    = K21_DG_V + DPiq_n05*(DVsDpi_n1*DPiDq_n1*1/(pi_n1-pi_n) - (Vs_n1-Vs_n)/(pi_n1-pi_n)^2*DPiDq_n1)' + (Vs_n1-Vs_n)/(pi_n1-pi_n)*1/2*D2PiDq2;
-                
+                    V_invariants_difference_too_small = false;
                 else
-                    % else use MP evaluation of gradient
-                    DG_Vint = DG_Vint + 1/p*this_problem.internal_potential_gradient(q_n05);
-                    K21_DG_V  = K21_DG_V  + 1/p*D2Vint_n05;
+                    V_invariants_difference_too_small = true;
                 end
                 
-                
+            end
+            
+            if V_invariants_difference_too_small
+                % else use MP evaluation of gradient
+                DG_Vint = this_problem.internal_potential_gradient(q_n05);
+                K21_DG_V = 1/2*D2Vint_n05;
             end
             
             % for the gradients of the constraints
@@ -118,35 +117,34 @@ classdef EMS_std < Integrator
                     % discrete gradient
                     DG_g(j,:)   = (gs_n1 - gs_n)/(zeta_n1 - zeta_n)*DzetaDq_n05';
                     K21_DG_g    = K21_DG_g + lambda_n1(j)*(DzetaDq_n05'*(DgsDzeta_n1*DzetaDq_n1*1/(zeta_n1-zeta_n) - (gs_n1 - gs_n)/(zeta_n1 - zeta_n)^2*DzetaDq_n1) + (gs_n1 - gs_n)/(zeta_n1 - zeta_n)*1/2*D2zetaDq2);
-                
+                    g_invariants_difference_too_small = false;
                 else
-                    % else use MP evaluation of gradient
-                    G_tmp     = this_problem.constraint_gradient(q_n05);
-                    DG_g(j,:) = G_tmp(j,:);
-                    K21_DG_g  = K21_DG_g  + lambda_n1(j)*this_problem.constraint_hessian(q_n05,j);
+                    g_invariants_difference_too_small = true;
+                    break
                 end
-                
-                %for the tangent matrix
-                
+                            
             end
             
-            %% Terms with Hessian of constraints
-            % Hessian of constraints are multiplied by LMs for each
-            % Constraint (avoid 3rd order tensor)
-            for i = 1:m
-                t_n05   = t_n05 + this_problem.constraint_hessian(q_n05,m)*lambda_n1(m);
+            if g_invariants_difference_too_small
+                % else use MP evaluation of gradient
+                G_n05 = this_problem.constraint_gradient(q_n05);
+                DG_g  = G_n05;
+                for j=1:this_problem.nConstraintInvariants
+                    D2g_Dq_n05 = this_problem.constraint_hessian(q_n05,j);
+                    K21_DG_g  = K21_DG_g  + 1/2*lambda_n1(j)*D2g_Dq_n05;
+                end
             end
-            
+                        
             %% Residual vector 
             resi = [qn1 - qn - h*IM*p_n05                                     ;
                     pn1 - pn + h*DVext_n05   + h*DG_Vint + h*DG_g'*lambda_n1  ;
                     g_n1                                                     ];
 
             %% Tangent matrix
-            tang = [eye(n)                                                  -h*0.5*IM      zeros(n,m) ;
-                    h*0.5*D2Vext_n05 + h*K21_DG_V + h*K21_DG_g              eye(n)         h*DG_g'   ; 
-                    G_n1                                                    zeros(n,m)'    zeros(m)  ];
-            %tang=[];
+            tang = [eye(n)                                        -h*0.5*IM      zeros(n,m);
+                    h*D2Vext_n05 + h*K21_DG_V + h*K21_DG_g        eye(n)         h*DG_g'   ; 
+                    G_n1                                          zeros(n,m)'    zeros(m)  ];
+        
         end
         
     end
