a5a559511dad669fda78446dd05d7ceed070d515
Philipp <philipp.kinon@student.kit.edu>
2021-03-29 14:50:01 +0200

    higher initial velocities

--- input/config_input_double_pendulum.m
+++ input/config_input_double_pendulum.m
@@ -2,7 +2,7 @@
 SYSTEM  = 'DoublePendulum';
 EXT_ACC = [0; 0; 9.81];
 Q_0     = [1; 0; 0; 2.5; 0; 0];
-V_0     = [0; 0.1; 0; 0; 0; 0];
+V_0     = [0; 1; 0; 0; 2; 0];
 MASS    = [1; 1.5];
 DIM     = 3;
 
@@ -20,8 +20,8 @@ TOLERANCE      = 1E-09;
 %% Postprocessing
 shouldAnimate   = false;
 plot_quantities = {'energy','energy_difference','angular_momentum','angular_momentum_difference','constraint_position','constraint_velocity'};
-should_export         = true;
-should_export_figures = true;
+should_export         = false;
+should_export_figures = false;
 export_path           = 'scratch/';
 
 %% Write variables into a .mat-File
--- input/config_input_pendulum.m
+++ input/config_input_pendulum.m
@@ -2,13 +2,12 @@
 SYSTEM = 'Pendulum';
 EXT_ACC = [0; 0; 9.81];
 Q_0     = [1; 0; 0];
-V_0     = [0; 0.1; 0];
+V_0     = [0; 1; 0];
 MASS    = 1;
 DIM     = 3;
 
 %% Integrator
-INTEGRATOR = 'GGL_VI_theta_B';
-INT_PARA = [1 0.5];
+INTEGRATOR = 'GGL_VI';
 DT    = 0.05;
 T_0   = 0;
 T_END = 10;
