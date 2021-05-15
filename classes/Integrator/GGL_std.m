4cdfed30a41d342b2343f079d28c3e1faed5325f
philipplk <philipp.kinon@student.kit.edu>
2021-05-15 13:46:44 +0200

    einheitliche q-Auswertung

--- classes/Integrator/GGL_std.m
+++ classes/Integrator/GGL_std.m
@@ -6,7 +6,7 @@ classdef GGL_std < Integrator
 % - independent momenta variables (Livens approach)
 %
 % - standard stabilisation scheme by Gear, Gupta & Leimkuhler applied to
-%   SHAKE method
+%   the constrained symplectic Euler B method
 %
 % - not derived from variational principle: constraints evaluated at
 %   t_{n+1} and ODE-RHS for q at t_{n+1} and for p at t_n
@@ -50,15 +50,15 @@ classdef GGL_std < Integrator
             qn1     = zn1(1:n);
             pn1     = zn1(n+1:2*n);
             lambdan = zn1(2*n+1:2*n+m);
-            gamman1 = zn1(2*n+m+1:end);
+            gamman  = zn1(2*n+m+1:end);
             G_n1    = this_problem.constraint_gradient(qn1);
             g_n1    = this_problem.constraint(qn1);
             
             % Hessian of constraints are multiplied by LMs for each
             % Constraint (avoid 3rd order tensor)
-            t_n1    = zeros(n);
+            t_n    = zeros(n);
             for i = 1:m
-                t_n1   = t_n1 + this_problem.constraint_hessian(qn1,i)*gamman1(i);
+                t_n   = t_n + this_problem.constraint_hessian(qn1,i)*gamman(i);
             end
             
             % Hessian of constraints are multiplied by inverse MassMat and
@@ -78,13 +78,13 @@ classdef GGL_std < Integrator
             DV_n   = this_problem.internal_potential_gradient(qn) + this_problem.external_potential_gradient(qn);
             
             %% Residual vector 
-            resi = [qn1 - qn - h*IM*pn1 + h*G_n1'*gamman1 ;
+            resi = [qn1 - qn - h*IM*pn1 + h*G_n'*gamman ;
                     pn1 - pn + h*DV_n   + h*G_n'*lambdan  ;
                     g_n1                                  ;
                     G_n1*IM*pn1                          ];
 
             %% Tangent matrix
-            tang = [eye(n) + h*t_n1             -h*IM          zeros(n,m)    h*G_n1'     ;
+            tang = [eye(n) + h*t_n             -h*IM          zeros(n,m)    h*G_n'     ;
                     zeros(n)                    eye(n)         h*G_n'        zeros(n,m)  ; 
                     G_n1                        zeros(n,m)'    zeros(m)      zeros(m)           ;
                     T_n1                        G_n1*IM        zeros(m)      zeros(m)    ];
