bfa6e36e5eafff46f064cae6e594864e2c47e670
José Luis Muñoz Reyes <122202491+JLMR29@users.noreply.github.com>
2023-08-17 14:37:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,5 @@ exclude_patterns = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+primary_domain = "mat"
