a8991f3a44354cb3eb07642b48fa279303b80041
José Luis Muñoz Reyes <122202491+JLMR29@users.noreply.github.com>
2023-08-17 11:21:07 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,8 +37,6 @@ extensions = [ 'sphinx.ext.duration',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# -- Options for HTML output
-html_theme = 'sphinx_rtd_theme'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,8 +48,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
