e3aaf372cb0170de6e03bc7eee3b115226995ec8
Philipp Kinon <philipp.kinon@kit.edu>
2024-03-11 17:22:22 +0100

    animate for top

--- classes/System/HeavyTopQuaternions.m
+++ classes/System/HeavyTopQuaternions.m
@@ -475,48 +475,62 @@ classdef HeavyTopQuaternions < System
         %% Animation method
             function give_animation(self, fig, this_simulation)
 
+                q = this_simulation.z(:, 1:4);
+                qa = q(1,:);
+                NT = size(q, 1);
 
-                DIM = self.DIM;
-                q1 = this_simulation.z(:, 1:DIM);
-                q2 = this_simulation.z(:, DIM+1:2*DIM);
-                q3 = this_simulation.z(:, 2*DIM+1:3*DIM);
-                q4 = this_simulation.z(:, 3*DIM+1:4*DIM);
-                xmin = min([min(q1(:, 1)), min(q2(:, 1)), min(q3(:, 1)), min(q4(:, 1))]);
-                ymin = min([min(q1(:, 2)), min(q2(:, 2)), min(q3(:, 2)), min(q4(:, 2))]);
-                zmin = min([min(q1(:, 3)), min(q2(:, 3)), min(q3(:, 3)), min(q4(:, 3))]);
-                xmax = max([max(q1(:, 1)), max(q2(:, 1)), max(q3(:, 1)), max(q4(:, 1))]);
-                ymax = max([max(q1(:, 2)), max(q2(:, 2)), max(q3(:, 2)), max(q4(:, 2))]);
-                zmax = max([max(q1(:, 3)), max(q2(:, 3)), max(q3(:, 3)), max(q4(:, 3))]);
-                NT = size(q1, 1);
+                %extract vector and scalar part form quaternion
+                q_vec = qa(2:4)';
+                q_scalar = qa(1);
+
+                q_hat = [0, -q_vec(3), q_vec(2);
+                    q_vec(3), 0, -q_vec(1);
+                    -q_vec(2), q_vec(1), 0];
+            
+                % transformation matrix
+                G_q = [-q_vec, q_scalar*eye(3) - q_hat];
+                E_q = [-q_vec, q_scalar*eye(3) + q_hat];
+                R_q = E_q*G_q';
+                
+                ampl_factor = 10;
+                a=ampl_factor*0.1;
+                r = a/2;
+                l=3/4*a;
+                
+                P1 = [0,0,a]';
+                P2 = [0,0,l]';
+
+                p1a = R_q*P1;
+                p2a = R_q*P2;
+                
+                d1 = R_q*[1;0;0];
+                d2 = R_q*[0;1;0];
+                d3 = R_q*[0;0;1];
+
+                h1 = mArrow3(p2a,p2a'+d1', 'facealpha', 0.5, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
+                h2 = mArrow3(p2a,p2a'+d2', 'facealpha', 0.5, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
+                h3 = mArrow3(p2a,p2a'+d3', 'facealpha', 0.5, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
+                hold on
+                % [X,Y,Z]=cylinder([0 1],1000);
+                % M=makehgtform('translate',[-1,-1,-1],'xrotate',pi/4,'yrotate',pi/4);
+                % h=surf(X,Y,Z,'Parent',hgtransform('Matrix',M),'LineStyle','none','FaceAlpha',0.3);  
+                
+                d(1,:) = zeros(3,1); 
+                d(2,:) = p1a;
+                [X3,Y3,Z3,~,~,~] = cone(atan(r/a)/(2*pi)*360,d,a);
+                surf(X3,Y3,Z3,'LineStyle','none','FaceColor',[0 150/255 130/255],'FaceAlpha',0.8);
 
                 axis equal
-                %axis([xmin, xmax, ymin, ymax, zmin, zmax]);
-                %xlabel('x');
-                %ylabel('y');
-                %zlabel('z');
-                %grid on;
-                axis off
+                xmin = -1.5;
+                xmax = 1.5;
+                ymin = -1.5;
+                ymax = 1.5;
+                zmin =-0.4;
+                zmax =1.6;
+                axis([xmin, xmax, ymin, ymax, zmin, zmax]);
 
-                xa1 = q1(1, 1);
-                xa2 = q2(1, 1);
-                xa3 = q3(1, 1);
-                xa4 = q4(1, 1);
-                ya1 = q1(1, 2);
-                ya2 = q2(1, 2);
-                ya3 = q3(1, 2);
-                ya4 = q4(1, 2);
-
-                if DIM == 3
-                    za1 = q1(1, 3);
-                    za2 = q2(1, 3);
-                    za3 = q3(1, 3);
-                    za4 = q4(1, 3);
-                else
-                    za1 = 0;
-                    za2 = 0;
-                    za3 = 0;
-                    za4 = 0;
-                end
+                grid on;
+                axis off;
 
                 for j = 1:NT
 
@@ -524,122 +538,42 @@ classdef HeavyTopQuaternions < System
                     hold on
 
                     %% Current position
-                    x1 = q1(j, 1);
-                    x2 = q2(j, 1);
-                    x3 = q3(j, 1);
-                    x4 = q4(j, 1);
+                    q_current = q(j,:);
+                    
+                    q_vec = q_current(2:4)';
+                    q_scalar = q_current(1);
 
-                    y1 = q1(j, 2);
-                    y2 = q2(j, 2);
-                    y3 = q3(j, 2);
-                    y4 = q4(j, 2);
+                    q_hat = [0, -q_vec(3), q_vec(2);
+                            q_vec(3), 0, -q_vec(1);
+                           -q_vec(2), q_vec(1), 0];
+            
+                    % transformation matrix
+                    G_q = [-q_vec, q_scalar*eye(3) - q_hat];
+                    E_q = [-q_vec, q_scalar*eye(3) + q_hat];
+                    R_q = E_q*G_q';
+    
+                    p1a = R_q*P1;
+                    p2a = R_q*P2;
 
-                    if DIM == 3
-                        z1 = q1(j, 3);
-                        z2 = q2(j, 3);
-                        z3 = q3(j, 3);
-                        z4 = q4(j, 3);
-                    else
-                        z1 = 0;
-                        z2 = 0;
-                        z3 = 0;
-                        z4 = 0;
-                    end
+                    d1 = R_q*[1;0;0];
+                    d2 = R_q*[0;1;0];
+                    d3 = R_q*[0;0;1];
 
-                    %% Reference sphere
-                    %                     plot3(xa1, ya1, za1, 'mo', 'MarkerSize', 10, 'MarkerEdgeColor', [0.75, 0, 0], 'MarkerFaceColor', [0.75, 0, 0]);
-                    %                     hold on
-                    %                     plot3(xa2, ya2, za2, 'mo', 'MarkerSize', 10, 'MarkerEdgeColor', [0.75, 0, 0], 'MarkerFaceColor', [0.75, 0, 0]);
-                    %                     hold on
-                    %                     plot3(xa3, ya3, za3, 'mo', 'MarkerSize', 10, 'MarkerEdgeColor', [0.75, 0, 0], 'MarkerFaceColor', [0.75, 0, 0]);
-                    %                     hold on
-                    %                     plot3(xa4, ya4, za4, 'mo', 'MarkerSize', 10, 'MarkerEdgeColor', [0.75, 0, 0], 'MarkerFaceColor', [0.75, 0, 0]);
-                    %                     hold on
-
-                    %% Reference constraint
-                    %                     xx3 = [xa1; xa2];
-                    %                     yy3 = [ya1; ya2];
-                    %                     zz3 = [za1; za2];
-                    %                     xxx3 = [xa3; xa4];
-                    %                     yyy3 = [ya3; ya4];
-                    %                     zzz3 = [za3; za4];
-                    %                     plot3(xx3, yy3, zz3, 'k', 'LineWidth', 2);
-                    %                     hold on
-                    %                     plot3(xxx3, yyy3, zzz3, 'k', 'LineWidth', 2);
-
-                    %% Reference position of the springs
-                    %                     xx_3 = [xa1; xa3];
-                    %                     yy_3 = [ya1; ya3];
-                    %                     zz_3 = [za1; za3];
-                    %                     xxxx_3 = [xa2; xa4];
-                    %                     yyyy_3 = [ya2; ya4];
-                    %                     zzzz_3 = [za2; za4];
-                    %
-                    %                     tmp  = gfx_springelement([xa1,ya1,za1],[xa3,ya3,za3],1,0.1,5);
-                    %                     tmp2 = gfx_springelement([xa2,ya2,za2],[xa4,ya4,za4],1,0.1,5);
-                    %
-                    %                     %plot3(xx_3, yy_3, zz_3, 'k--');
-                    %                     plot3(tmp(1,:),tmp(2,:),tmp(3,:),'LineWidth',1.5);
-                    %                     hold on
-                    %                     %plot3(xxxx_3, yyyy_3, zzzz_3, 'k--');
-                    %                     plot3(tmp2(1,:),tmp2(2,:),tmp2(3,:),'LineWidth',1.5);
-
-                    %% current position of the mass
-                    %hold on
-                    %if DIM == 3
-                    %    plot3(q1(1:j, 1), q1(1:j, 2), q1(1:j, 3), 'k');
-                    %    plot3(q2(1:j, 1), q2(1:j, 2), q2(1:j, 3), 'k');
-                    %    plot3(q3(1:j, 1), q3(1:j, 2), q3(1:j, 3), 'k');
-                    %    plot3(q4(1:j, 1), q4(1:j, 2), q4(1:j, 3), 'k');
-                    %else
-                    %    plot3(q1(1:j, 1), q1(1:j, 2), zeros(j, 1), 'k');
-                    %    plot3(q2(1:j, 1), q2(1:j, 2), zeros(j, 1), 'k');
-                    %    plot3(q3(1:j, 1), q3(1:j, 2), zeros(j, 1), 'k');
-                    %    plot3(q4(1:j, 1), q4(1:j, 2), zeros(j, 1), 'k');
-                    %end
-                    plot3(x1, y1, z1, 'mo', 'MarkerSize', 40, 'MarkerEdgeColor', '#4477AA', 'MarkerFaceColor', '#4477AA');
-                    plot3(x2, y2, z2, 'mo', 'MarkerSize', 40, 'MarkerEdgeColor', '#4477AA', 'MarkerFaceColor', '#4477AA');
-                    plot3(x3, y3, z3, 'mo', 'MarkerSize', 40, 'MarkerEdgeColor', '#4477AA', 'MarkerFaceColor', '#4477AA');
-                    plot3(x4, y4, z4, 'mo', 'MarkerSize', 40, 'MarkerEdgeColor', '#4477AA', 'MarkerFaceColor', '#4477AA');
-
-                    %grid on
-
-                    %% current position of the constraint
-                    x_3 = [x1; x2];
-                    y_3 = [y1; y2];
-                    z_3 = [z1; z2];
-                    xxx3 = [x3; x4];
-                    yyy3 = [y3; y4];
-                    zzz3 = [z3; z4];
-                    plot3(x_3, y_3, z_3, 'k', 'linewidth', 2);
-                    plot3(xxx3, yyy3, zzz3, 'k', 'linewidth', 2);
-
-                    %% Current position of the springs
-                    xx3 = [x1; x3];
-                    yy3 = [y1; y3];
-                    zz3 = [z1; z3];
-                    xxxx3 = [x2; x4];
-                    yyyy3 = [y2; y4];
-                    zzzz3 = [z2; z4];
-
-                    tmp3 = gfx_springelement([x1, y1, z1], [x3, y3, z3], 1, 0.05, 7);
-                    tmp4 = gfx_springelement([x2, y2, z2], [x4, y4, z4], 1, 0.05, 7);
-
-                    %plot3(xx3, yy3, zz3, 'k--');
-                    plot3(tmp3(1, :), tmp3(2, :), tmp3(3, :), 'k', 'LineWidth', 1.5);
+                    h1 = mArrow3(p2a,p2a'+d1', 'facealpha', 0.8, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
+                    h2 = mArrow3(p2a,p2a'+d2', 'facealpha', 0.8, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
+                    h3 = mArrow3(p2a,p2a'+d3', 'facealpha', 0.8, 'color', [70/255 100/255 170/255], 'stemWidth', 0.02);
                     hold on
-                    plot3(tmp4(1, :), tmp4(2, :), tmp4(3, :), 'k', 'LineWidth', 1.5);
-                    %plot3(xxxx3, yyyy3, zzzz3, 'k--');
+                    % [X,Y,Z]=cylinder([0 1],1000);
+                    % M=makehgtform('translate',[-1,-1,-1],'xrotate',pi/4,'yrotate',pi/4);
+                    % h=surf(X,Y,Z,'Parent',hgtransform('Matrix',M),'LineStyle','none','FaceAlpha',0.3);  
+                    
+                    d(1,:) = zeros(3,1); 
+                    d(2,:) = p1a;
+                    [X3,Y3,Z3,~,~,~] = cone(atan(r/a)/(2*pi)*360,d,a);
+                    surf(X3,Y3,Z3,'FaceColor',[0 150/255 130/255],'FaceAlpha',0.8, 'EdgeColor',[0 0 0], 'LineWidth',1);
 
-
-                    if DIM == 2
-                        view(0, 90)
-                    else
-                        view(136, 23)
-                    end
-
+                    view(136, 23)
                     drawnow
-
                     print(gcf, strcat('snapshot_', num2str(j)), '-dpng')
 
                 end
