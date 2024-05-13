c272b004d21c567514dc4f0c7ceb5dcaa1a19dd9
Philipp Kinon <philipp.kinon@kit.edu>
2024-05-13 11:40:09 +0200

    new startscript for new example

--- start_metis_single_analysis.m
+++ start_metis_single_analysis.m
@@ -27,8 +27,8 @@ addpath(genpath(fileparts(which(mfilename))));
 % Metis creates objects from input-file
 %[simulation, system, integrator, solver] = Metis('input/published/MUBO_kinon_betsch_2024/single_analysis_RedundantMassSpring', 1, 1);
 %[simulation, system, integrator, solver] = Metis('input/published/MUBO_kinon_betsch_2024/single_analysis_SpringPendulumPolar', 1, 1);
-[simulation, system, integrator, solver] = Metis('input/published/MUBO_kinon_betsch_2024/quat_freeRB/single_analysis_rigidBodyRotatingQuat', 1, 1);
-%[simulation, system, integrator, solver] = Metis('input/single_analysis_HeavyTopQuat', 1, 1);
+%[simulation, system, integrator, solver] = Metis('input/published/MUBO_kinon_betsch_2024/quat_freeRB/single_analysis_rigidBodyRotatingQuat', 1, 1);
+[simulation, system, integrator, solver] = Metis('input/published/MUBO_kinon_betsch_2024/single_analysis_ClosedLoopMBS', 1, 1);
 
 %% METIS solver
 % Solve system with chosen solver and integration scheme
