68bad14c7baf43b81fffcdafa53c49423ebf4d21
Philipp Kinon <philipp.kinon@kit.edu>
2025-04-30 11:39:05 +0200

    adapt old integrators II

--- classes/Integrator/CSE_B.m
+++ classes/Integrator/CSE_B.m
@@ -32,7 +32,7 @@ classdef CSE_B < Integrator
 
         end
 
-        function [resi, tang] = compute_resi_tang(self, zn1, zn, this_system)
+        function [resi, tang] = compute_resi_tang(self, zn1, zn, this_system, ~)
             % Computes residual vector & tangent matrix
             %
             % :param zn1: state vector for next time step
